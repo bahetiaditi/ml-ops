040d32930ba792bc1be4e871298f2537969847aa
aditi <bahetiaa@gmail.com>
2023-09-05 22:40:58 +0530

    Removed error from prev class code

--- exp.py
+++ exp.py
@@ -1,150 +1,47 @@
-"""
-================================
-Recognizing hand-written digits
-================================
-
-This example shows how scikit-learn can be used to recognize images of
-hand-written digits, from 0-9.
-
-"""
-
-# Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
-# License: BSD 3 clause
-
-# Standard scientific Python imports
 import matplotlib.pyplot as plt
 
 # Import datasets, classifiers and performance metrics
-from sklearn import datasets, metrics, svm
-from sklearn.model_selection import train_test_split
+from sklearn import metrics, svm
+from utils import preprocess_digits, train_model, split_train_dev_test,read_digits,predict_and_eval
+gamma_ranges = [0.001,0.01,0.1,1,10,100]
+C_ranges = [0.1,1,2,3,5,10]
 
-###############################################################################
-# Digits dataset
-# --------------
-#
-# The digits dataset consists of 8x8
-# pixel images of digits. The ``images`` attribute of the dataset stores
-# 8x8 arrays of grayscale values for each image. We will use these arrays to
-# visualize the first 4 images. The ``target`` attribute of the dataset stores
-# the digit each image represents and this is included in the title of the 4
-# plots below.
-#
-# Note: if we were working from image files (e.g., 'png' files), we would load
-# them using :func:`matplotlib.pyplot.imread`.
+#Dataset
+X,y=read_digits()
 
-digits = datasets.load_digits()
+#Data splitting : train,test,dev
+X_train,X_test,X_dev,y_train,y_test,y_dev = split_train_dev_test(X, y, test_size=0.3, dev_size=0.2)
 
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, label in zip(axes, digits.images, digits.target):
-    ax.set_axis_off()
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title("Training: %i" % label)
-##############################################################################
-#Writing the function for train test split:
+#data preprocessing
+X_train=preprocess_digits(X_train)
+X_dev=preprocess_digits(X_dev)
+X_test=preprocess_digits(X_test)
 
-
-def split_train_dev_test(X, y, test_size, dev_size):
-    # Split data into train, development, and test sets
-    X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=test_size, 
-shuffle=False)
-    X_dev, X_test, y_dev, y_test = train_test_split(X_temp, y_temp, test_size=dev_size, 
-shuffle=False)
-    
-    return X_train, X_dev, X_test, y_train, y_dev, y_test
-
-##############################################################################
-def predict_and_eval(model, X_test, y_test):
-    # Getting model predictions on the test set
-    global predicted_test
-    predicted_test = model.predict(X_test)
-
-    # Evaluate the model on the test set
-    classification_report = metrics.classification_report(y_test, predicted_test)
-    confusion_matrix = metrics.confusion_matrix(y_test, predicted_test)
-    
-    return classification_report, confusion_matrix
-###############################################################################
-# Classification
-# --------------
-#
-# To apply a classifier on this data, we need to flatten the images, turning
-# each 2-D array of grayscale values from shape ``(8, 8)`` into shape
-# ``(64,)``. Subsequently, the entire dataset will be of shape
-# ``(n_samples, n_features)``, where ``n_samples`` is the number of images and
-# ``n_features`` is the total number of pixels in each image.
-#
-# We can then split the data into train and test subsets and fit a support
-# vector classifier on the train samples. The fitted classifier can
-# subsequently be used to predict the value of the digit for the samples
-# in the test subset.
-
-# flatten the images
-n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1))
-
-# Split data into train, development, and test sets using the new function
-X_train, X_dev, X_test, y_train, y_dev, y_test = split_train_dev_test(data, digits.target, 
-test_size=0.4, dev_size=0.25)
-
-# Create a classifier: a support vector classifier
-clf = svm.SVC(gamma=0.001)
-# Learn the digits on the train subset
-clf.fit(X_train, y_train)
-
-#Use the predict_and_eval function to evaluate the model on the test set
-classification_report, confusion_matrix = predict_and_eval(clf, X_test, y_test)
+#hyperparameter tuning
+#take all combinations of gamma and c
+best_acc = -1
+best_model = None
+for curr_gamma in gamma_ranges:
+    for curr_C in C_ranges:
+        #train model with curr gamma and curr C
+        curr_model = train_model(X_train,y_train,{'gamma':curr_gamma,'C' : curr_C},model_type="svm")
+        #get some performance metric on dev set
+        curr_accuracy,predicted=predict_and_eval(curr_model,X_dev,y_dev)
+        #select the hparams that give best perf on dev set
+        if curr_accuracy>best_acc:
+            print(f"New best accuracy {curr_accuracy}")
+            best_acc=curr_accuracy
+            optimal_gamma=curr_gamma
+            optimal_C=curr_C
+            best_model = curr_model
+print(f"Optimal C is {optimal_C} , Optimal gamma is {optimal_gamma}")
 
 
-###############################################################################
-# Below we visualize the first 4 test samples and show their predicted
-# digit value in the title.
-
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, prediction in zip(axes, X_test, predicted_test):
-    ax.set_axis_off()
-    image = image.reshape(8, 8)
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title(f"Prediction: {prediction}")
-
-###############################################################################
-# :func:`~sklearn.metrics.classification_report` builds a text report showing
-# the main classification metrics.
-
-print(
-    f"Classification report for classifier {clf}:\n"
-    f"{metrics.classification_report(y_test, predicted_test)}\n"
-)
-
-###############################################################################
-# We can also plot a :ref:`confusion matrix <confusion_matrix>` of the
-# true digit values and the predicted digit values.
-
-disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted_test)
-disp.figure_.suptitle("Confusion Matrix")
-print(f"Confusion matrix:\n{disp.confusion_matrix}")
-
-plt.show()
-
-###############################################################################
-# If the results from evaluating a classifier are stored in the form of a
-# :ref:`confusion matrix <confusion_matrix>` and not in terms of `y_true` and
-# `y_pred`, one can still build a :func:`~sklearn.metrics.classification_report`
-# as follows:
+# Predict the value of the digit on the test subset
+# 6.Predict and Evaluate 
+accuracy,predicted = predict_and_eval(best_model, X_test, y_test)
 
 
-# The ground truth and predicted lists
-y_true = []
-y_pred = []
-cm = disp.confusion_matrix
 
-# For each cell in the confusion matrix, add the corresponding ground truths
-# and predictions to the lists
-for gt in range(len(cm)):
-    for pred in range(len(cm)):
-        y_true += [gt] * cm[gt][pred]
-        y_pred += [pred] * cm[gt][pred]
 
-print(
-    "Classification report rebuilt from confusion matrix:\n"
-    f"{metrics.classification_report(y_true, y_pred)}\n"
-)
+
--- utils.py
+++ utils.py
@@ -1,1 +1,45 @@
-#Just experimenting with git branches
+import matplotlib.pyplot as plt
+from sklearn import svm,datasets
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.metrics import accuracy_score
+
+
+def read_digits():
+    digits=datasets.load_digits()
+    x = digits.images
+    y = digits.target 
+    return x,y
+
+
+
+def preprocess_digits(dataset):
+    n_samples = len(dataset)
+    data = dataset.reshape((n_samples, -1))
+    return data
+
+def split_data(X,y,test_size=0.5,random_state=1):
+    X_train, X_test, y_train, y_test = train_test_split(
+     X, y, test_size=test_size, shuffle=False
+    )
+    return X_train, X_test, y_train, y_test
+
+
+def split_train_dev_test(X, y, test_size, dev_size):
+    X_train, X_test, y_train, y_test = split_data(X, y, test_size=test_size)
+    X_train, X_dev, y_train, y_dev = split_data(X_train, y_train, test_size=dev_size)
+    return X_train, X_test,X_dev, y_train, y_test, y_dev
+
+def train_model(x,y,model_params,model_type="svm"):
+    if model_type == 'svm':
+        clf = svm.SVC(**model_params)
+    clf.fit(X, y)
+    return clf
+
+
+def predict_and_eval(model, X_test, y_test):
+    # Getting model predictions on the test set
+    predicted = model.predict(X_test)
+    return metrics.accuracy_score(y_test,predicted),predicted
+
+    
