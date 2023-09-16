ef32e1f0fa2717f0698e207f3758b40c393f87ba
aditi <bahetiaa@gmail.com>
2023-09-16 14:41:27 +0530

    pytest

---
+++ test_sample.py
@@ -0,0 +1,25 @@
+from utils import get_all_h_param_comb
+
+def test_for_hparam_combinations_count():
+    gamma_list=[0.001,0.01,0.1,1]
+    C_list=[1,10,100,1000]
+    h_params={}
+    h_params['gamma']=gamma_list
+    h_params['C']=C_list
+    h_params_combinations = get_all_h_param_comb(h_params)
+
+    assert len(h_params_combinations) == len(gamma_list)*len(C_list)
+
+def test_for_hparam_combinations_values():
+    gamma_list=[0.001,0.01]
+    C_list=[1]
+    h_params={}
+    h_params['gamma']=gamma_list
+    h_params['C']=C_list
+    h_params_combinations = get_all_h_param_comb(h_params)
+
+    expected_param_combo_1 = {'gamma':0.001,'C':1}
+
+
+    assert expected_param_combo_1 in h_params_combinations
+
