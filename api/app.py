f83d6a7b5b534d5bb8cca044c0a0c4d2c9043ebf
aditi <bahetiaa@gmail.com>
2023-11-07 21:50:23 +0530

    Quiz 3

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/model', methods=['POST'])
 def pred_model():
-    js = request.get_json()
+    """js = request.get_json()
     image_path_1 = js['image1']
     image_path_2 = js['image2']
    
@@ -13,4 +13,6 @@ def pred_model():
     prediction_2 = model.predict(image_path_2)
     are_same = np.array_equal(prediction_1, prediction_2)
 
-    return jsonify({'result': are_same})
+    return jsonify({'result': are_same})"""
+
+    return("False")
